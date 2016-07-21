bf378a4302df696bec820043f9167729a4f792b9
MAriaADF <hellenl2012@gmail.com>
2016-07-21 17:36:10 -0600

    commits #2

--- WindowsFormsApplication1/WebBrowser .cs
+++ WindowsFormsApplication1/WebBrowser .cs
@@ -13,10 +13,10 @@ using System.Threading;
 
 namespace WindowsFormsApplication1
 {
-    public partial class Form1 : Form
+    public partial class frmWebBrowser : Form
     {
         
-        public Form1()
+        public frmWebBrowser()
         {
             InitializeComponent();
             PerformanceCounter perfCpuCount = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
@@ -24,29 +24,18 @@ namespace WindowsFormsApplication1
             PerformanceCounter perfMemCount = new PerformanceCounter("Memory", "Available MBytes");
             perfMemCount.NextValue();
 
-
             PerformanceCounter perfUptimeCount = new PerformanceCounter("System", "System Up Time");
             perfUptimeCount.NextValue();
 
             int currentCpuPercentage = (int)perfCpuCount.NextValue();
             int currentAvailableMemory = (int)perfMemCount.NextValue();
-            textBox1.Text = Convert.ToString(currentCpuPercentage);
+            lblCPU.Text = Convert.ToString(currentCpuPercentage);
             int RAM = currentAvailableMemory;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void btnBuscar_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn_buscar_Click(object sender, EventArgs e)
-        {
-
+            webBrowser1.Navigate(txtDireccion.Text);
         }
     }
 }
